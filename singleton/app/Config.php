1138140ae032747a05b5f2f80e511f39e227df61
Math.dlcrt <mathias.delcourt4@etu.univ-lorraine.fr>
2025-03-10 16:11:25 +0100

    Implémentation template singleton

--- singleton/app/Config.php
+++ singleton/app/Config.php
@@ -3,3 +3,27 @@
 # TODO: Créer une classe Config en Singleton
 
 namespace App;
+
+class Config
+{
+    private static $instance = null;
+    private static $settings;
+
+    private function __construct()
+    {
+        self::$settings = require(__DIR__ . '/../config/config.php');
+    }
+
+    public static function getInstance(): self
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function get(string $key)
+    {
+        return self::$settings[$key] ?? null;
+    }
+}
--- singleton/public/index.php
+++ singleton/public/index.php
@@ -1,7 +1,14 @@
 <?php
 require('../vendor/autoload.php');
 
+use App\Config;
+
 
 # TODO: Récuperer une instance de Config
 # Afficher une valeur contenu dans config.php
 # Récupérer une seconde instance de Config et vérifié que les deux instances sont identiques
+
+$config = Config::getInstance();
+$config2 = Config::getInstance();
+echo ($config == $config2) ? 'true' : 'false';
+echo "<br>" . $config->get('apiKey');
